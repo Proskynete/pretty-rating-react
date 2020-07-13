a661cd14767aaaf73c3334e5ae73361f9f46f815
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-13 18:08:53 -0400

    Insert a new line in createIcon helper

--- src/helper/create-icon.helper.js
+++ src/helper/create-icon.helper.js
@@ -7,9 +7,11 @@ export const createIcon = ({ rating, icons, iconsNumber }) => {
 	for (i; i >= 1; i -= 1) {
 		iconsToPrint.push(complete);
 	}
+
 	if (i >= 0.5 && i <= 1) {
 		iconsToPrint.push(half);
 	}
+  
 	while (iconsToPrint.length < iconsNumber) {
 		iconsToPrint.push(empty);
 	}
