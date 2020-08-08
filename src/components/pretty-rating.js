18a5e1135a90e30abf69a54dec593fcdccc6d938
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-08 18:47:54 -0400

    Allow new prop - setColors

--- src/components/pretty-rating.js
+++ src/components/pretty-rating.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 import { createIcon } from '../helper/create-icon.helper';
 
 const PrettyRating = (props) => {
-	const { rating, icons, iconsNumber } = props;
+	const { rating, icons, iconsNumber, setColors } = props;
 	const iconsToRender = createIcon({ rating, icons, iconsNumber });
 
-	return iconsToRender.map((icon) => (
-		<i key={Math.random()} className={`${icon}`} aria-hidden='true' />
-	));
+	return iconsToRender.map((icon) => {
+		return <i key={Math.random()} className={`${icon}`} aria-hidden='true' />;
+	});
 };
 
 PrettyRating.defaultProps = {
 	iconsNumber: 5,
+	setColors: ['#000', '#000', '#000'],
 };
 
 PrettyRating.propTypes = {
 	rating: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
 	icons: PropTypes.shape({ complete: '', half: '', empty: '' }).isRequired,
+	setColors: PropTypes.shape([]),
 	iconsNumber: PropTypes.number,
 };
 
