cc9be9f4b73a8e7fb40154ed2b5f70318fc83f9d
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-01-15 15:48:58 -0300

    add: allow change value

--- src/components/pretty-rating.tsx
+++ src/components/pretty-rating.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IconProp, library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -15,20 +15,35 @@ export const PrettyRating = ({
 	max = defaultValues.max,
 	colors = defaultValues.colors,
 }: PrettyRatingInterface.Props) => {
-	const iconsToRender = createIcon({ value, icons, max });
+	const [_value, setValue] = useState<number>(value);
+	const [iconsToRenderState, setIconsToRenderState] =
+		useState<PrettyRatingInterface.CreateIconResponse[]>();
+
+	useEffect(() => {
+		const iconsToRender = createIcon({ value: _value, icons, max });
+		setIconsToRenderState(iconsToRender);
+	}, [_value]);
 
 	return (
-		<div>
-			{iconsToRender.map((icon, i) => {
+		<>
+			{iconsToRenderState?.map((icon, i) => {
 				return (
 					<FontAwesomeIcon
 						key={i}
-						icon={
-							typeof icon.name === 'string'
-								? (icon.name as IconProp)
-								: icon.name
-						}
+						icon={typeof icon.name === 'string' ? (icon.name as IconProp) : icon.name}
+						onMouseEnter={() => {
+							const iconsToRender = createIcon({ value: i + 1, icons, max });
+							setIconsToRenderState(iconsToRender);
+						}}
+						onMouseLeave={() => {
+							const iconsToRender = createIcon({ value: _value, icons, max });
+							setIconsToRenderState(iconsToRender);
+						}}
+						onClick={() => {
+							setValue(i + 1);
+						}}
 						style={{
+							cursor: 'pointer',
 							color: customStyles({
 								current: icon.name,
 								icons,
@@ -39,6 +54,6 @@ export const PrettyRating = ({
 					/>
 				);
 			})}
-		</div>
+		</>
 	);
 };
